1677b655d84fe11ac6ba358fec6f2270284b58b1
Mark Chandler <mark@chanchan.io>
2019-09-28 17:29:33 -0400

    add prism-ghcolors theme

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,3 +1,6 @@
 // custom typefaces
-import "typeface-montserrat"
-import "typeface-merriweather"
+import 'typeface-montserrat'
+import 'typeface-merriweather'
+
+// prism theme
+import 'prism-themes/themes/prism-ghcolors.css'
