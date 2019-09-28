da2424272b1ed628164e2de0beb1f74fa688396f
Mark Chandler <mark@chanchan.io>
2019-09-28 17:57:31 -0400

    switch to Github typography

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,6 +1,8 @@
+// global styles
+import './src/styles/globals.css'
+
 // custom typefaces
-import 'typeface-montserrat'
-import 'typeface-merriweather'
+import 'typeface-source-code-pro'
 
 // prism theme
 import 'prism-themes/themes/prism-ghcolors.css'
