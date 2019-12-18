2fda973ad18b0efcf1337c98f005de457757d056
Mark Chandler <mark@chanchan.io>
2019-12-17 22:48:08 -0500

    create gatsby-theme-series-data

---
+++ packages/gatsby-theme-series-data/gatsby-node.js
@@ -0,0 +1,22 @@
+exports.createSchemaCustomization = ({actions, schema}) => {
+  const {createTypes} = actions
+
+  createTypes(`
+    interface SeriesPost @nodeInterface {
+      id: ID!
+      title: String!
+      slug: String!
+      excerpt: String
+      content: String!
+    }
+  `)
+
+  createTypes(`
+    type Series implements Node {
+      id: ID!
+      name: String!
+      slug: String!
+      items: [SeriesPost]!
+    }
+  `)
+}
