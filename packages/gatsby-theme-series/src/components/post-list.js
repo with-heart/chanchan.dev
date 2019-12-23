eea171cfa39d07ca9798d19d29ef419f7bb1b75f
Mark Chandler <mark@chanchan.io>
2019-12-23 17:42:14 -0500

    remove unused console.log

--- packages/gatsby-theme-series/src/components/post-list.js
+++ packages/gatsby-theme-series/src/components/post-list.js
@@ -5,7 +5,6 @@ export default ({currentPost = {}, posts}) => {
   return (
     <ol>
       {posts.map(post => {
-        console.log('draft', post.draft)
         return (
           <li key={post.id}>
             {post.id === currentPost.id || post.draft ? (
